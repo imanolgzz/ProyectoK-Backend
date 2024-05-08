a15cd64e146f9f7e6fa8b57eea84c3c0ba621e30
Imanol González <117108948+imanolgzz@users.noreply.github.com>
2024-05-08 09:05:31 -0600

    minor tests

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
-const express = require('express');
-const app = express();
-const port = 5000;
+const express = require('express')
+const {Client} = require('pg')
+const app = express()
+const port = 5000
+
+const client = new Client({
+  user: "postgres",
+  password: "120820061418",
+  host: "192.168.137.138",
+  port: "5432",
+  database: "projectK"
+})
+
+client.connect()
+  .then(() => {console.log("Connnected to PostgreSQL database")})
+  .catch((err) => {console.log("Error connecting to PostgreSQL database", err)})
 
 app.get('/', (req, res) => {
-  console.log("Route / is working fine");
-  res.status(200).json({message: "It´s working fine"});
+  console.log("Route / is working fine")
+  res.status(200).json({message: "It´s working fine"})
 });
+
+const users = require('./routes/users')
+app.use('/users', users)
 
 app.listen(5000);
