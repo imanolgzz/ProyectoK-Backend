0666e6068cdea6a6b9eaec1b40edc7ee79fdefe6
Carlos Martinez <carlosh.mtz@hotmail.com>
2024-05-08 20:56:15 -0600

    new structure declaration and implementation separated and new routes

--- routes/users.js
+++ routes/users.js
@@ -1,39 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const client = require("../helpers/postgres.ts");
+const { getUsers, getUserById, createUser } = require("../controllers/users.js");
 
-router.get("/", (req, res) => {
-  client.query("SELECT * FROM users", (err, result) => {
-    if (err) {
-      console.log("Error executing query", err);
-      res.status(500).json({ message: "Error executing query" });
-    } else {
-      console.log("Query result", result.rows);
-      res.status(200).json(result.rows);
-    }
-  });
-});
-
-router.get("/:id", (req, res) => {
-  console.log("Request params", req.params.id);
-  const id = req.params.id;
-  client.query(
-    "SELECT * FROM users WHERE user_id = $1",
-    [id],
-    (err, result) => {
-      if (err) {
-        console.log("User does not exist", err);
-        res.status(500).json({ message: "Error executing query" });
-      } else {
-        if (result.rows.length === 0) {
-          res.status(404).json({ message: "this user does not exist" });
-        } else {
-          console.log("Query result", result.rows);
-          res.status(200).json(result.rows);
-        }
-      }
-    }
-  );
-});
+//get all users
+router.get("/", getUsers);
+//get user by id
+router.get("/:id", getUserById);
+//create new user NOT READY
+router.post("/", createUser);
 
 module.exports = router;
